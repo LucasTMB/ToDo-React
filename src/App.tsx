97a3cd0616b7d22947443a98a3870b2bcae15ee1
LucasTMB <lucas.tmb708@gmail.com>
2023-07-12 23:09:25 -0300

    Criando o formulário e iniciando a lista de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
+//CSS
+import styles from "./App.module.css";
+
+// Interface
+import { ITask } from './interfaces/Task';
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div className="App">
-      <h1>To do</h1>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText='Criar tarefa' taskList={taskList} setTaskList={setTaskList} />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList taskList={taskList} />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
